418edd5bc1751a8a1074c0533b4409a2ea4f1667
Joseph Jen <jjen.mis@gmail.com>
2022-01-13 16:13:53 -0600

    Removed EDGE and Opera option

--- src/test/java/helpers/BrowserFabric.java
+++ src/test/java/helpers/BrowserFabric.java
@@ -5,45 +5,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.opera.OperaDriver;
+//import org.openqa.selenium.opera.OperaDriver;
 
 public class BrowserFabric {
     public static WebDriver getDriver(BrowserType browserType){
-        switch (browserType) {
-            case FIREFOX -> {
+        if (browserType == BrowserType.FIREFOX) {
                 return getFirefoxDriver();
             }
-            case OPERA -> {
-                return getOperaDriver();
-            }
-            case EDGE -> {
-                return getEdgeDriver();
-            }
-            default -> {
-                return getChromeDriver();
-            }
+            return getChromeDriver();
         }
-    }
 
     private static WebDriver getChromeDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver(options);
-    }
-
-    private static WebDriver getEdgeDriver() {
-        WebDriverManager.edgedriver().setup();
-        return new EdgeDriver();
-    }
-
-    private static WebDriver getOperaDriver() {
-        WebDriverManager.operadriver().setup();
-        return new OperaDriver();
     }
 
     private static WebDriver getFirefoxDriver() {
@@ -53,3 +33,48 @@ public class BrowserFabric {
         return new FirefoxDriver(options);
     }
 }
+
+
+//public class BrowserFabric {
+//    public static WebDriver getDriver(BrowserType browserType){
+//        switch (browserType) {
+//            case FIREFOX -> {
+//                return getFirefoxDriver();
+//            }
+//            case OPERA -> {
+//                return getOperaDriver();
+//            }
+//            case EDGE -> {
+//                return getEdgeDriver();
+//            }
+//            default -> {
+//                return getChromeDriver();
+//            }
+//        }
+//    }
+//
+//    private static WebDriver getChromeDriver() {
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+//        options.addArguments("--headless");
+//        WebDriverManager.chromedriver().setup();
+//        return new ChromeDriver(options);
+//    }
+//
+//    private static WebDriver getEdgeDriver() {
+//        WebDriverManager.edgedriver().setup();
+//        return new EdgeDriver();
+//    }
+//
+//    private static WebDriver getOperaDriver() {
+//        WebDriverManager.operadriver().setup();
+//        return new OperaDriver();
+//    }
+//
+//    private static WebDriver getFirefoxDriver() {
+//        FirefoxOptions options = new FirefoxOptions();
+//        options.addArguments("--headless");
+//        WebDriverManager.firefoxdriver().setup();
+//        return new FirefoxDriver(options);
+//    }
+//}
